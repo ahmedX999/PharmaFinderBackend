a46ca38e97a66edb34d9c214329921603ade2c5c
ahmedX999 <aaittaleb.354@gmail.com>
2023-06-02 21:04:55 +0100

    first commit

---
+++ src/main/java/com/localisation/ahmedApplication.java
@@ -0,0 +1,13 @@
+package com.localisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ahmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ahmedApplication.class, args);
+	}
+
+}
